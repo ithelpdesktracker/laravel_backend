35b01f4893b2c9699638cc559c559363ae53541c
Victor Cornejo <vcornejo@ip-172-31-76-45.ec2.internal>
2018-03-22 23:13:03 +0000

    adding editted files for the auth token

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -25,7 +25,9 @@ class UserController extends Controller
 
         if(Auth::attempt(['email' => request('email'), 'password' => request('password')])){
             $user = Auth::user();
-            $success['token'] =  $user->createToken('MyApp')->accessToken;
+            $success['ucid']=$user->ucid;
+            $success['job_title']=$user->job_title;
+            $success['api_token'] =  $user->createToken('MyApp')->accessToken;
             return response()->json(['success'=>$success], 200);
         }
         else{
