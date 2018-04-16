5d277fb25b2efab04eaeec98c29fa9285f710cd0
Victor Cornejo <vcornejo@ip-172-31-76-45.ec2.internal>
2018-04-16 00:49:22 +0000

    added patch to accepted criteria

--- public/index.php
+++ public/index.php
@@ -8,7 +8,7 @@
  */
 define('LARAVEL_START', microtime(true));
 header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS, PATCH');
 header('Access-Control-Allow-Headers: *');
 //header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
 /*
