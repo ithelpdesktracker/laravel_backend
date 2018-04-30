e754f07112d6fb9d5e7ae11e8abf51a58f04ea19
Victor Cornejo <vcornejo@ip-172-31-76-45.ec2.internal>
2018-04-30 01:30:22 +0000

    query chnage for frontend

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -14,7 +14,7 @@ class DashboardController extends Controller
 {
     public function dashOne()
     {
-    
+    /*
           $request=DB::table('tech_issue_assignments')
           ->Select("tech_issue_assignments.tech_ucid","tickets.status", DB::raw("count(tickets.status) as Total"))
       		->from('tech_issue_assignments')
@@ -24,18 +24,42 @@ class DashboardController extends Controller
       		->groupBy('tech_issue_assignments.tech_ucid')
       		->groupBy('tickets.status')
       		->get();
+         
+         
+         $request =DB::select('tech_issue_assignments.tech_ucid')
+          		->distinct()
+          		->addSelect(DB::raw('sum(case when tickets.status= open then 1 else 0 end) as Open'))
+          		->addSelect(DB::raw('sum(case when tickets.status = resolved then 1 else 0 end) as Resolved'))
+          		->addSelect(DB::raw('sum(case when tickets.status =unresolved then 1 else 0 end) as Unresolved'))
+          		->from('tech_issue_assignments')
+          		->join('tickets', function($join) {
+          			$join->on('tech_issue_assignments.issue_id', '=', 'tickets.iss_id');
+          			})
+          		->groupBy('tech_issue_assignments.tech_ucid')
+          		->get();
+             */
+        $request =DB::table('tech_issue_assignments')
+        ->Select('tech_issue_assignments.tech_ucid')
+  		  ->distinct()  
+        ->addSelect(DB::raw('sum(case when tickets.status= "open" then 1 else 0 end) as Open'))
+    		->addSelect(DB::raw('sum(case when tickets.status = "resolved" then 1 else 0 end) as Resolved'))
+    		->addSelect(DB::raw('sum(case when tickets.status ="unresolved" then 1 else 0 end) as Unresolved'))
+       		->from('tech_issue_assignments')
+      		->rightjoin('tickets', function($join) {
+      			$join->on('tech_issue_assignments.issue_id','=', 'tickets.iss_id');
+      			})
+      		->groupBy('tech_issue_assignments.tech_ucid')
+      	
+         ->get();
+       
+
 
         return response()->json([$request], 200);
 
     }
     public function unresolved()
     {
-        $request=DB::table('tickets')
-                ->Select(DB::raw("count(tickets.status) as Total"))
-            		->from('tickets')
-            		->where('status', '=', 'unresolved')
-            		->get();
-           return response()->json([$request], 200);
+        
     //select count(status) as Total_Unresolved from tickets where status ='unresolved';
 
         $request = DB::table('tickets')
